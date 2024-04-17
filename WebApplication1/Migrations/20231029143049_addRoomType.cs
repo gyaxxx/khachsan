499105e8d352ccca5855a77f50ab62a8dc648e0f
Nghia Pham <gyax241@gmail.com>
2024-04-17 20:51:10 +0700

    commit

---
+++ WebApplication1/Migrations/20231029143049_addRoomType.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class addRoomType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoomType",
+                columns: table => new
+                {
+                    RoomTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Bed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    linkimg1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    linkimg2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    linkimg3 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    linkimg4 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    linkimg5 = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomType", x => x.RoomTypeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomType");
+        }
+    }
+}
---
+++ WebApplication1/Models/RoomType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class RoomType
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomTypeId{ get; set; }
+        public string RoomTypeName {  get; set; }
+        public int Area {  get; set; }
+        public float Price {  get; set; }
+        public string Bed{ get; set; }
+        public string linkimg1{get; set; }
+        public string linkimg2 { get; set; }
+        public string linkimg3 { get; set; }
+        public string linkimg4 { get; set; }
+        public string linkimg5 { get; set; }
+    }
+}
